932331443b0a07307ca215c624c756109155a40d
agustinn1233 <agustinn1233@hotmail.com>
2020-11-24 19:24:05 -0300

    [App] MeliAppXv2
    Correction in status bar color.

--- app/src/main/java/com/android/mercadolibre/meliappxv2/MainActivity.kt
+++ app/src/main/java/com/android/mercadolibre/meliappxv2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.mercadolibre.meliappxv2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 import com.android.mercadolibre.meliappxv2.src.ui.ProductSearchFragment
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Not use dark mode.
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        // Status bar color
+        window.statusBarColor = ContextCompat.getColor(this, R.color.black)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
